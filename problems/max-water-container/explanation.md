a2dc2b97e85738a9fc04ccb0ff4c866fb3e293dc
ArshinMQ <fatemmaqu@gmail.com>
2026-02-28 04:50:40 +0330

    2026, Feb 27

---
+++ problems/max-water-container/explanation.md
@@ -0,0 +1,5 @@
+- Link: https://neetcode.io/problems/max-water-container/question
+- Difficulity: Medium
+
+## 2026, Feb 27
+Greedy problems are just so easy.
---
+++ problems/max-water-container/solves/2026_feb27.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        max_area = 0
+        l = 0
+        r = len(heights) - 1
+        while l < r:
+            vl = heights[l]
+            vr = heights[r]
+            curr_area = min(vl, vr) * (r - l)
+            max_area = max(max_area, curr_area)
+            if vl < vr:
+                l += 1
+            else:
+                r -= 1
+
+        return max_area
